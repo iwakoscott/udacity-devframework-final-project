c1fd53d1e20d9a82b16c1d60935d3d1956472ae1
Satoshi Scott Iwako <iwakoscott@gmail.com>
2017-08-06 13:14:42 -0700

    add functionality to add new restaurants.

--- finalproject.py
+++ finalproject.py
@@ -16,11 +16,25 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 app = Flask(__name__)
 
+# Main Page
 @app.route('/')
 @app.route('/restaurants/')
 def displayRestaurants():
     items = session.query(Restaurant).all()
     return render_template('restaurants.html', items=items)
+
+# Add New Restaurant
+@app.route('/restaurants/new/', methods=['GET', 'POST'])
+def addNewRestaurant():
+    if request.method == 'POST':
+        newRestaurant = Restaurant(name=request.form['newRestaurantName'])
+        session.add(newRestaurant)
+        session.commit()
+        return redirect(url_for('displayRestaurants'))
+    else:
+        return render_template('addrestaurant.html')
+
+
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
