4b902b9eca4077fa1149a1390b7f10db04a9f79e
Satoshi Scott Iwako <iwakoscott@gmail.com>
2017-08-05 23:03:02 -0700

    add basic scripts necessary to use flask.

--- finalproject.py
+++ finalproject.py
@@ -9,3 +9,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 from sqlalchemy import distinct
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+app = Flask(__name__)
+
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='localhost', port=5000)
